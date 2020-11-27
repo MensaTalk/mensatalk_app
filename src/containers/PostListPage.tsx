e0c3e100c442f595642eb8f36051cd00934519f3
abteilung6 <moeller.tilman@gmail.com>
2020-11-27 19:43:53 +0100

    dispatch getAllPost and select posts

--- src/containers/PostListPage.tsx
+++ src/containers/PostListPage.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
 import {Text} from 'react-native';
 
-const PostListPage = () => {
-  return (
-    <>
-      <Text>Hello, I am your cat!</Text>
-    </>
-  );
+import {getAllPosts} from '../selectors/posts';
+import {getPostsStart} from '../slices/posts';
+
+const PostListPage: React.FC = () => {
+  const dispatch = useDispatch();
+
+  const {posts} = useSelector(getAllPosts);
+
+  useEffect(() => {
+    dispatch(getPostsStart());
+  }, [dispatch]);
+
+  return <>{posts ? posts.map((post) => <Text>{post.title}</Text>) : ''}</>;
 };
 
 export default PostListPage;
