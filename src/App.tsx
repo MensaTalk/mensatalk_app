084bd09cd5ce856303cf0813bf5c5e4511baf00d
abteilung6 <moeller.tilman@gmail.com>
2020-11-27 18:53:16 +0100

    move to new src dir and remove content

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, ScrollView, View, StatusBar} from 'react-native';
+
+import {Header} from 'react-native/Libraries/NewAppScreen';
+
+declare const global: {HermesInternal: null | {}};
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView>
+          <Header />
+          <View>Edit</View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
