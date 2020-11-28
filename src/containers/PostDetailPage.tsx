784a51fc874d18a2e3f78d21d37b25aeef096acd
abteilung6 <moeller.tilman@gmail.com>
2020-11-28 13:18:11 +0100

    add websocket receiving and sending

--- src/containers/PostDetailPage.tsx
+++ src/containers/PostDetailPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Button, Text} from 'react-native';
 import {StackScreenProps} from '@react-navigation/stack';
 
@@ -6,7 +6,30 @@ import {RootStackParamList} from '../navigation/RootNavigation';
 
 type Props = StackScreenProps<RootStackParamList, 'PostListPage'>;
 
+const ws = new WebSocket('ws://192.168.2.113:3030');
+
 const PostDetailPage: React.FC<Props> = ({route, navigation}: Props) => {
+  const [messages, setMessages] = useState<String[]>([]);
+
+  useEffect(() => {
+    ws.onopen = () => {
+      console.log('Websocket opened.');
+    };
+  }, []);
+
+  ws.onmessage = (e) => {
+    console.log(`Received: ${e.data}`);
+    setMessages([...messages, e.data]);
+  };
+
+  ws.onerror = (e) => {
+    console.log(`Error: ${e.message}`);
+  };
+
+  ws.onclose = (e) => {
+    console.log(e.code, e.reason);
+  };
+
   return (
     <>
       <Text>{route.name}</Text>
@@ -14,6 +37,10 @@ const PostDetailPage: React.FC<Props> = ({route, navigation}: Props) => {
         title="Go to PostListPage"
         onPress={() => navigation.navigate('PostListPage')}
       />
+      <Button title="send" onPress={() => ws.send('helo')} />
+      {messages.map((message, index) => (
+        <Text key={index}>Message: {message}</Text>
+      ))}
     </>
   );
 };
