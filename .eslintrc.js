805631c5f9fed524616a36d8031941b2cb54445a
abteilung6 <moeller.tilman@gmail.com>
2020-11-29 19:15:36 +0100

    add prettier rule end of line, caused by windows and mac

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,4 +3,12 @@ module.exports = {
   extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
 };
