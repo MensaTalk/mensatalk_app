d4cfc271dca89669194a34615dabfe6b4cf28046
abteilung6 <moeller.tilman@gmail.com>
2020-11-29 13:22:24 +0100

    add message reducer to root reducer

--- src/rootReducer.ts
+++ src/rootReducer.ts
@@ -1,9 +1,11 @@
 import {combineReducers} from '@reduxjs/toolkit';
 
 import roomReducer from './slices/rooms';
+import messageReducer from './slices/messages';
 
 const combinedReducers = combineReducers({
   rooms: roomReducer,
+  messages: messageReducer,
 });
 
 export default combinedReducers;
