d7dcbc0cb8a5bd257cb1648dd7a84bba14af5a2d
abteilung6 <moeller.tilman@gmail.com>
2020-12-02 16:47:42 +0100

    add types SignUpUserInterface SignInUserInterface

--- src/types.ts
+++ src/types.ts
@@ -13,3 +13,14 @@ export interface UserInterface {
   id: number;
   name: string;
 }
+
+export interface SignUpUserInterface {
+  username: string;
+  email: string;
+  password: string;
+}
+
+export interface SignInUserInterface {
+  username: string;
+  password: string;
+}
