8ba63a5569e0b571d5188b81f4c76098b3ec173a
abteilung6 <moeller.tilman@gmail.com>
2020-12-09 17:56:23 +0100

    redirect to rooms after sign in

--- src/containers/SignUpPage.tsx
+++ src/containers/SignUpPage.tsx
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {StackScreenProps} from '@react-navigation/stack';
+import {useSelector, useDispatch} from 'react-redux';
 
 import {SignUserInterface} from '../types';
 import {RootStackParamList} from '../navigation/RootNavigation';
+import {signUpUserStart} from '../slices/user';
+import {getUser} from '../selectors/user';
 import SignUpForm from '../components/Form/SignUpForm';
 
 type Props = StackScreenProps<RootStackParamList, 'RoomListPage'>;
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const SignUpPage: React.FC<Props> = ({route, navigation}: Props) => {
+  const dispatch = useDispatch();
+  const [process, setProcess] = useState(false);
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const {token, isLoading, error} = useSelector(getUser);
+
+  useEffect(() => {
+    if (process && token) {
+      navigation.navigate('RoomListPage');
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [process, token]);
+
   const onSignUp = (signUser: SignUserInterface) => {
-    navigation.navigate('RoomListPage');
+    console.log(signUser);
+    setProcess(true);
+    dispatch(signUpUserStart(signUser));
   };
 
   return (
