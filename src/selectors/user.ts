3ad426fbf981f4fdae0232eb653d88f195f3faff
abteilung6 <moeller.tilman@gmail.com>
2020-12-09 17:31:08 +0100

    add selector for user infos

---
+++ src/selectors/user.ts
@@ -0,0 +1,12 @@
+import {createSelector} from '@reduxjs/toolkit';
+
+import {AppState} from '../store';
+import {initialState} from '../slices/user';
+
+const userDomain = (state: AppState) => state.user || initialState;
+
+export const getUser = createSelector([userDomain], (userState) => ({
+  token: userState.token,
+  isLoading: userState.isLoading,
+  error: userState.error,
+}));
