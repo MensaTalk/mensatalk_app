db457edcbd936d298d23ac55ea710ef1ec531217
abteilung6 <moeller.tilman@gmail.com>
2020-12-09 22:04:14 +0100

    useEffect on selectedRoom use Client and ServerMessage

--- src/containers/RoomDetailPage.tsx
+++ src/containers/RoomDetailPage.tsx
@@ -6,14 +6,16 @@ import io from 'socket.io-client';
 import {getMessagesStart, addMessage} from '../slices/messages';
 import {getSelectedRoom} from '../selectors/rooms';
 import {getAllMessages} from '../selectors/messages';
-import {ActualMessage, MessageInterface} from '../types';
+import {ClientMessage, ServerMessage, MessageInterface} from '../types';
 
 import {RootStackParamList} from '../navigation/RootNavigation';
 import Chat from '../components/Chat/Chat';
 
 type Props = StackScreenProps<RootStackParamList, 'RoomDetailPage'>;
 
-const socket = io.connect('http://192.168.2.110:9001');
+let clientSocket: SocketIOClient.Socket;
+
+// const socket = io.connect('http://192.168.2.113:9001');
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const RoomDetailPage: React.FC<Props> = ({route, navigation}: Props) => {
@@ -24,22 +26,33 @@ const RoomDetailPage: React.FC<Props> = ({route, navigation}: Props) => {
 
   useEffect(() => {
     selectedRoom && dispatch(getMessagesStart(selectedRoom.id));
-    socket.on('connect', () => {});
   }, [dispatch, selectedRoom]);
 
-  socket.on('message', (data: ActualMessage) => {
-    console.log(`Received: ${data.payload}`);
-    const receivedMessage: MessageInterface = {
-      id: NaN,
-      textMessage: data.payload,
-      created_at: '',
-    };
-    dispatch(addMessage(receivedMessage));
-  });
+  useEffect(() => {
+    if (selectedRoom) {
+      console.log('lets connect');
+      clientSocket = io.connect('http://192.168.2.113:9001', {
+        query: {
+          roomId: 1,
+          name: 'alice',
+        },
+      });
+      clientSocket.on('message', (serverMessage: ServerMessage) => {
+        console.log(`Received: ${serverMessage.payload}`);
+        const receivedMessage: MessageInterface = {
+          id: NaN,
+          textMessage: serverMessage.payload,
+          created_at: '',
+        };
+        dispatch(addMessage(receivedMessage));
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [selectedRoom]);
 
   const handleOnSendText = (text: string) => {
-    const actualMessage: ActualMessage = {payload: text};
-    socket.emit('message', actualMessage);
+    const clientMessage: ClientMessage = {payload: text};
+    clientSocket.emit('message', clientMessage);
   };
 
   return (
