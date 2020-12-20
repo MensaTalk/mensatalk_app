31deb43e976df78d906b3b82bfb0438ea4a4eab7
abteilung6 <moeller.tilman@gmail.com>
2020-12-20 16:29:25 +0100

    display selected Profile in ProfileDetailPage

--- src/containers/ProfileDetailPage.tsx
+++ src/containers/ProfileDetailPage.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import {useSelector} from 'react-redux';
 import {StackScreenProps} from '@react-navigation/stack';
+
+import {selectSelectedProfile} from '../selectors/profiles';
 
 import {RootStackParamList} from '../navigation/RootNavigation';
 import ProfileDetail from '../components/Profile/ProfileDetail';
@@ -8,13 +11,8 @@ type Props = StackScreenProps<RootStackParamList, 'ProfileDetailPage'>;
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const ProfileDetailPage: React.FC<Props> = ({route, navigation}: Props) => {
-  return (
-    <>
-      <ProfileDetail
-        profile={{id: 1, username: 'name', age: 99, interests: '', status: ''}}
-      />
-    </>
-  );
+  const selectedProfile = useSelector(selectSelectedProfile);
+  return <>{selectedProfile && <ProfileDetail profile={selectedProfile} />}</>;
 };
 
 export default ProfileDetailPage;
