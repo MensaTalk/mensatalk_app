611ad75e845138f4b70b6cd852645174693b41db
abteilung6 <moeller.tilman@gmail.com>
2020-12-20 13:45:03 +0100

    load profiles from backend if profilelistpage is active

--- src/containers/ProfileListPage.tsx
+++ src/containers/ProfileListPage.tsx
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
 import {StackScreenProps} from '@react-navigation/stack';
+
+import {getUserIds, getSelectedRoom} from '../selectors/rooms';
 
 import {RootStackParamList} from '../navigation/RootNavigation';
 import ProfileList from '../components/Profile/ProfileList';
+import {getUser} from '../selectors/user';
+import {selectRoomProfiles} from '../selectors/profiles';
+import {getProfileStart} from '../slices/profiles';
 
 type Props = StackScreenProps<RootStackParamList, 'ProfileListPage'>;
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const ProfileListPage: React.FC<Props> = ({route, navigation}: Props) => {
+  const dispatch = useDispatch();
+  const user = useSelector(getUser);
+  const selectedRoom = useSelector(getSelectedRoom);
+  const userIds = useSelector(getUserIds);
+  const roomProfiles = useSelector(selectRoomProfiles);
+
+  useEffect(() => {
+    if (selectedRoom && user) {
+      userIds.forEach((userId) =>
+        dispatch(
+          getProfileStart({
+            token: user.token ? user.token : '',
+            payload: userId,
+          }),
+        ),
+      );
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [dispatch, selectedRoom]);
+
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const handleOnClick = (profileId: number) => {
     navigation.navigate('ProfileDetailPage');
@@ -15,7 +41,7 @@ const ProfileListPage: React.FC<Props> = ({route, navigation}: Props) => {
 
   return (
     <>
-      <ProfileList profiles={[]} onClick={handleOnClick} />
+      <ProfileList profiles={roomProfiles} onClick={handleOnClick} />
     </>
   );
 };
